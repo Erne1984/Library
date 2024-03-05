7f2f37ad1a46633e701badffe9583ff13e64932f
ernesto <ernestofamorim@gmail.com>
2024-03-05 15:05:02 -0300

    modal

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,16 @@
+const modal = document.querySelector("dialog");
+const addBtn = document.querySelector("#add-btn");
+const closeBtn = document.querySelector("#close-btn");
 const myLibrary = [];
 
-function Book(title, author, pages, ) {
+function Book(title, author, pages,) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.readed = readed;
-  this.info = function(){
-    const isReadedInfo = readed? "was read": "not read yet";
- 
+  this.info = function () {
+    const isReadedInfo = readed ? "was read" : "not read yet";
+
     return `${this.title} by ${this.author}, ${this.pages} pages, ${isReadedInfo}`
   }
 }
@@ -15,3 +18,11 @@ function Book(title, author, pages, ) {
 function addBookToLibrary() {
   // do stuff here
 }
+
+addBtn.addEventListener("click", () => {
+  modal.showModal()
+})
+
+closeBtn.addEventListener("click", () => {
+  modal.close()
+})
