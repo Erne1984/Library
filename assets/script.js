b03c583ab5cb946aa9ed29b2cfea2dee93e368d7
ernesto <ernestofamorim@gmail.com>
2024-03-07 09:58:59 -0300

    function addBook

--- assets/script.js
+++ assets/script.js
@@ -3,13 +3,17 @@ const addBookBtn = document.querySelector("#add-book");
 const openModalBtn = document.querySelector("#add-btn");
 const submitBtn = document.querySelector("#submit-btn");
 
+const containerCards = document.querySelector(".cards-container");
 const inputTitle = document.querySelector("#title");
 const inputAuthor = document.querySelector("#author");
 const inputPages = document.querySelector("#pages");
 const checkReaded = document.querySelector("#read");
 
-
-const myLibrary = [];
+const myBooks = [
+  new Book("Metamorphosis", "Franz Kafka", 82, true),
+  new Book("O Alienista", "Machado de Assis", 80, true),
+  new Book("O Cavaleiro da Esperança", "Jorge Amado", 366, false),
+];
 
 function Book(title, author, pages, readed) {
   this.title = title;
@@ -21,18 +25,69 @@ function Book(title, author, pages, readed) {
 function addBookToLibrary() {
   let newBook = new Book(inputTitle.value, inputAuthor.value, inputPages.value.toString(), checkReaded.checked);
 
-  console.log(newBook)
+  let card = document.createElement("div");
+
+  card.classList.add("card");
+
+  for (let attribute in newBook) {
+    let contentDiv = document.createElement("div");
+
+    if (attribute === "readed") {
+      contentDiv.textContent = newBook[attribute] ? "Read" : "Not Read";
+    } else {
+      contentDiv.textContent = newBook[attribute];
+    }
+
+    contentDiv.classList.add(attribute);
+    card.appendChild(contentDiv);
+  }
+
+  eraseFields();
+
+  myBooks.push(newBook);
+
+  containerCards.appendChild(card);
 }
 
 function validatingInput() {
 
 }
 
+function eraseFields() {
+  inputTitle.textContent = "";
+  inputAuthor.textContent = "";
+  inputPages.textContent = "";
+  checkReaded.checked = false;
+}
+
+function fillDataBook() {
+  for (let book of myBooks) {
+    let card = document.createElement("div");
+    card.classList.add("card");
+
+    for (let attribute in book) {
+      let contentDiv = document.createElement("div");
+
+      if (attribute === "readed") {
+        contentDiv.textContent = book[attribute] ? "Read" : "Not Read";
+      } else {
+        contentDiv.textContent = book[attribute];
+      }
+
+      contentDiv.classList.add(attribute);
+      card.appendChild(contentDiv);
+    }
+
+    containerCards.appendChild(card);
+  }
+}
+
+fillDataBook()
+
 openModalBtn.addEventListener("click", () => {
   modal.showModal();
 })
 
 submitBtn.addEventListener("click", () => {
-  console.log(addBookToLibrary());
   modal.close();
 })
